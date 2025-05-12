46be903f401a0c8b0ba284364abde27ede90cf2a
Ammar Abdulhadi <ammarhadimjm@gmail.com>
2025-05-12 20:17:35 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.js");
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/style.css";
+import "@fortawesome/fontawesome-free/js/all.min";
+
+// const tooltipTriggerList = document.querySelectorAll(
+//   '[data-bs-toggle="tooltip"]'
+// );
+// const tooltipList = [...tooltipTriggerList].map(
+//   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+// );
+//summary 4 above beneath
+document
+  .querySelectorAll('[data-bs-toggle="tooltip"]')
+  .forEach((item) => new bootstrap.Tooltip(item));
+
+console.log("hello world");
+console.log("My name is Ammar");
